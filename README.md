96a6c513243004f6eb1d72d08e14d59a27790825
임성민 <lsmorigin@gmail.com>
2018-04-04 21:17:37 +0900

    초기 커밋
    - 시작!

---
+++ README.md
@@ -0,0 +1,2 @@
+# Welcome to SungminWorld !
+## 개인 포트폴리오용 게임 입니다
---
+++ Source/SungminWorld/SungminWorldGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SungminWorldGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASungminWorldGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASungminWorldGameMode();
+};
+
+
+
